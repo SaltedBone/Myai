319438c5b294bebaf2d45a6db325e959dcf4054f
Salted <115632872+SaltedBone@users.noreply.github.com>
2023-06-27 19:46:26 -0400

    Create navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,6 @@
+// This is a sample component for a navigation bar
+// You will need to implement this based on your application's needs
+
+function Navbar() {
+    // Render your navigation bar here
+}
