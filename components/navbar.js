21d8bf850c77702ecbc917f63cc286aac4218c44
Salted <115632872+SaltedBone@users.noreply.github.com>
2023-06-27 19:51:44 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,9 @@
-// This is a sample component for a navigation bar
-// You will need to implement this based on your application's needs
-
 function Navbar() {
-    // Render your navigation bar here
+    const navbar = document.createElement('div');
+    navbar.innerHTML = `
+        <button id="function1">Function 1</button>
+        <button id="function2">Function 2</button>
+        <button id="function3">Function 3</button>
+    `;
+    return navbar;
 }
